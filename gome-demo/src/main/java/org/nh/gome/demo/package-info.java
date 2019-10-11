5890a6ecccfbf0f6e8b12c4bf4a793967beff7e1
yindanqing <yindanqing925@163.com>
2019-10-11 09:37:57 +0800

    extend

--- gome-demo/src/main/java/org/nh/gome/demo/package-info.java
+++ gome-demo/src/main/java/org/nh/gome/demo/package-info.java
@@ -3,6 +3,6 @@
  *
  * @author yindanqing
  * @description 国美自写demo
- * @date 2019/6/6 16:02
+ * @date 2019/10/11 16:02
  */
 package org.nh.gome.demo;
---
+++ xyy-demo/src/main/java/org/nh/xyy/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * gome
+ *
+ * @author yindanqing
+ * @description xyy自写demo
+ * @date 2019/7/29 16:02
+ */
+package org.nh.xyy.demo;
