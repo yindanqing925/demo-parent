d0fb9d0a48fb840380ec7f72fcd6914d506d7b77
yindanqing <yindanqing925@163.com>
2019-12-28 03:01:48 +0800

    新增算法注释;

--- gome-demo/src/main/java/org/nh/gome/demo/algo/Solution.java
+++ gome-demo/src/main/java/org/nh/gome/demo/algo/Solution.java
@@ -16,8 +16,12 @@ public class Solution {
     }
 
     public int evalRPN(String[] tokens) {
+        //声明栈
         Stack<Integer> stack = new Stack<>();
         for (String token : tokens) {
+            // 匹配符号
+            // 如果命中, 则推出栈顶两个元素用当前符号进行运算, 栈顶第一个元素在运算符右边, 第二个元素在运算符左边(先进后出)
+            // 运算结果直接压入栈顶, 待下一次出栈运算使用(并不一定是下一个符号)
             if ("+".equals(token)) {
                 int rightNum = stack.pop();
                 int leftNum = stack.pop();
@@ -35,6 +39,7 @@ public class Solution {
                 int leftNum = stack.pop();
                 stack.push(leftNum / rightNum);
             } else {
+                //如果没有命中符号, 则默认为待操作数, 直接压入栈顶, 不做异常处理
                 stack.push(Integer.parseInt(token));
             }
         }
--- xyy-demo/src/main/java/org/nh/xyy/demo/algo/IsValid.java
+++ xyy-demo/src/main/java/org/nh/xyy/demo/algo/IsValid.java
@@ -17,27 +17,34 @@ public class IsValid {
         System.out.println(new IsValid().isValid("([)]"));
     }
 
-    private Map<Character, Character> map = new HashMap<>();
+    // 初始化符号容器
+    private Map<Character, Character> map = new HashMap<>(4);
     {
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
     }
+    // 声明栈
     private Stack<Character> stack = new Stack<>();
+
     public boolean isValid(String s) {
-        int res = 0;
+        // 遍历字符串数组
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
+            // 如果map的values中包含当前符号, 直接压入栈顶
+            if (map.values().contains(c)) {
                 stack.push(c);
             } else {
+                // 验证当前栈是否为空, 如果为空, 则条件一定不满足
                 if (stack.empty()){
                     return false;
                 }
+                //推出栈顶元素, 如果和当前符号在集合中的映射关系不符合, 则条件一定不满足
                 if(stack.pop() != map.get(c)){
                     return false;
                 }
             }
         }
+        //当所有元素遍历完成时, 栈一定为空, 如果栈不为空, 则条件一定不满足
         return stack.empty();
     }
 
