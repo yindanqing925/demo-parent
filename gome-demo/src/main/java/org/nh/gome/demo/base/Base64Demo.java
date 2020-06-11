0d64b7c22ac160b41a468558d2bdf12e014b5aa3
yindanqing <yindanqing@gome.com.cn>
2020-06-11 16:11:41 +0800

    123

---
+++ gome-demo/src/main/java/org/nh/gome/demo/base/Base64Demo.java
@@ -0,0 +1,14 @@
+package org.nh.gome.demo.base;
+
+/**
+ * @program: Base64Demo.java
+ * @description:
+ * @author: yindanqing
+ * @create: 2020/6/10 20:35
+ */
+public class Base64Demo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ gome-demo/src/main/java/org/nh/gome/demo/interfacedefault/Man.java
@@ -0,0 +1,15 @@
+package org.nh.gome.demo.interfacedefault;
+
+/**
+ * @program: Man.java
+ * @description:
+ * @author: yindanqing
+ * @create: 2020/5/30 11:13
+ */
+public interface Man {
+
+    default String talk(){
+        return "吹逼";
+    }
+
+}
